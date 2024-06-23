e150ddde0d046eb8546e3ac6f671ec495310b074
AveryHills_0 <110232710+tanishadas1212@users.noreply.github.com>
2024-06-23 13:45:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-bCFkvNC2/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded')
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,  results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
